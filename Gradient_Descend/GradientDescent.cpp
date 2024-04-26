30caf5e38726270e4a47257bc225b53de5bd3970
duy <doubled020504@gmail.com>
2024-04-27 00:28:01 +0700

    Finish FGD

--- Gradient_Descend/GradientDescent.cpp
+++ Gradient_Descend/GradientDescent.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 using namespace std; 
-const double gamma = 0.1;
-const double alpha = 0.9;
+
+const double gamma = 0.001;
+const double alpha = 0.1;
 const double e = 0.00001;
-double deriv(float x, char f){
-    if (f == 'f')
-    return 2*((2*x)/(x*x+10)+1)*(log(x*x+10)+x-5)+2;
-    else
-    return 2*((2*x)/(x*x+15)+1)*(log(x*x+15)+x-15)+1;
+
+double f1(double x) {
+    return 2*exp(pow(x,5)-pow(x,3)) - 5*pow(x,3) - x + log(x+45) + 20;
 }
-double deriv2(float x, char f){
-    if (f == 'f')
-    return 2*(2/(x*x+10)-(4*x*x)/((x*x+10)*(x*x+10)))*(log(x*x+10)+x-5)+2*pow((2*x)/(x*x+10)+1,2);
-    else 
-        return 2*(2/(x*x+15)-(4*x*x)/((x*x+15)*(x*x+15)))*(log(x*x+15)+x-15)+2*pow((2*x)/(x*x+15)+1,2);
-    }
-double newton(double x, int n, char f){
-    for (int i=0;i<n;i++){
-        x = x - deriv(x,f)/deriv2(x,f);
+
+double f2(double x) {
+    return 3*exp(pow(x,5)-pow(x,4)) + pow(x,2) - 20*x + log(x+25) - 10;
+}
+
+double deriv(double x, double (*f)(double)){
+    double h = 0.00001;
+    return (f(x+h) - f(x-h)) / (2*h);
+}
+
+double grad(double x, int n, double (*f)(double)){
+    double x_new;
+    for (int i=0;i<n;i++)
+    {
+        x_new = x - gamma*deriv(x,f);
+        if (abs(x-x_new)<e) break;
+        x = x_new;
     }
     return x;
 }
@@ -34,19 +41,17 @@ double grad_momentum(double x, int n, double e, char f){
     }
     return x;
 }
-double grad(double x, int n, char f){
-    double x_new, x0;
-    for (int i=0;i<n;i++)
-    {
-        x_new = x - gamma*deriv(x,f);
-        if (abs(x-x_new)<0.00001) break;
-        x = x_new;
+
+double newton(double x, int n, char f){
+    for (int i=0;i<n;i++){
+        x = x - deriv(x,f)/deriv2(x,f);
     }
     return x;
 }
+
 int main(){
-    int n = 1000;
-    double x;
+    int n = 10000;
+    double x = 0;
     char f;
     cin >> x;
     cin >> f;
